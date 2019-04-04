ea7d5add1b4754794b90e675de4d8aaa000011ee
Adm <Adm@192.168.20.53>
2019-04-04 14:13:17 -0400

    Main class added

---
+++ DakarRaceCIT260FP/src/DakarRace.java
@@ -0,0 +1,17 @@
+/**
+ * This is a simple program to simulate a Dakar Race
+ * For mor information about the Darkar you con enter to www.dakar.com
+ */
+
+/**
+ * @author Pavel Shvets
+ *         Noldis Chumacero
+ *
+ */
+public class DakarRace {
+
+	public static void main(String[] args) {
+		System.out.println("Wellcome to Darkar Race");
+	}
+
+}
