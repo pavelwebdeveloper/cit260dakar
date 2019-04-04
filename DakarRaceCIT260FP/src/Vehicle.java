084dd6e6de710c0027e3e7e9bee5b2390911351e
Adm <Adm@192.168.20.53>
2019-04-04 14:24:07 -0400

    The super class vehicle

---
+++ DakarRaceCIT260FP/src/Vehicle.java
@@ -0,0 +1,48 @@
+/**
+ * This is the Supper Class Vehicle.
+ * All the vehicles in the Darkar Race derive from this one.
+ */
+
+/**
+ * @author Pavel and Noldis
+ *
+ */
+public class Vehicle {
+
+	/**
+	 * 
+	 */
+	
+	// Every vehicle in the race have a number assigned and need to be show in the sides of the vehicle
+	int number;
+	
+	// The vehicles distinguish according of the number of wheels. Cars = 4, Bikes = 2, Trucks = 6, 8, 12
+	int numberOfWheels;
+	
+	public Vehicle() {
+	
+	}
+	
+	// Setters
+	void setNumber(int number)
+	{
+		this.number = number;
+	}
+	
+	void setNumberOfWheels(int numberOfWheels)
+	{
+		this.numberOfWheels = numberOfWheels;
+	}
+	
+	//Getters
+	
+	int getNumber()
+	{
+		return this.number;
+	}
+	
+	int getNumberOfWheels()
+	{
+		return this.numberOfWheels;
+	}
+}
