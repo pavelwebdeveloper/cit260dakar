7457d867ea87ce9f659cf84c4bad15d0f2243cbd
Cracken <Cracken@DESKTOP-CQIPE63>
2019-04-06 17:26:53 -0400

    Main clases

--- DakarRaceCIT260FP/src/DakarRace.java
+++ DakarRaceCIT260FP/src/DakarRace.java
@@ -11,7 +11,19 @@
 public class DakarRace {
 
 	public static void main(String[] args) {
-		System.out.println("Wellcome to Darkar Race");
+		System.out.println("Welcome to Darkar Race");
+		System.out.println("Creating cars");
+		// Call a _for_ creating all the cars
+		System.out.println("Creating bikes");
+		// Call a _for_ creating all the bikes		
+		System.out.println("Creating trucks");
+		// Call a _for_ creating all the trucks
+		System.out.println("Show the vehicles");
+		// Call a _for_ showing all the cars, trucks and bikes
+		// this call uses Polymorphism to show toString() in the objects
+		System.out.println("Run!!!!");
+		//Call the run in every object
+		System.out.println("Show the first places");
+		//Call for the three vehicles in every category
 	}
-
 }
